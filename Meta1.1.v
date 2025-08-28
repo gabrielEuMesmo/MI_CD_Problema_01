5cc4a6c4fcd000e3131e8d8300663bbe23f89aad
gabrielEuMesmo <95380319+gabrielEuMesmo@users.noreply.github.com>
2025-08-27 21:43:04 -0300

    Add files via upload

---
+++ Meta1.1.v
@@ -0,0 +1,33 @@
+module Teste(A,B,C,D,S0,S1,Co);
+input A, B, C, D;
+output S0, S1,S2, Co;
+wire w1;
+
+MeioSomador(A, C, w1, S0);
+SomadorCompleto(B, D, w1, C0, S1);
+
+
+
+endmodule
+
+module MeioSomador(A,B, Co, S);
+input A,B;
+output Co,S;
+
+xor (S, A, B);
+and (Co, A, B);
+endmodule 
+
+module SomadorCompleto(A,B, Ci,Co, S);
+
+input A, B, Ci;
+output Co, S;
+wire AoB, Cio,AeB, CieA;
+
+xor (AoB, A, B);
+xor (S, AoB, Ci);
+
+and (AeB, A, B);
+and (CieA, A, Ci);
+or (Co, AeB, CieA);
+endmodule 
